4a47b7d153973313ea6336ebdfd1b369692fbe38
MiltonValenzuela <miltvalenz@hotmail.com>
2019-11-23 16:18:58 -0400

    Initial configuration of project

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+const App = () => {
+    <Router>
+        <Route path="/" exact componet={}/>
+    </Router>
+}
