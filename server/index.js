9901b0d01fe1cbc8df72cb6c09d87603fdcbc136
MiltonValenzuela <miltvalenz@hotmail.com>
2019-11-23 23:05:28 -0400

    Join Component

--- server/index.js
+++ server/index.js
@@ -9,9 +9,26 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+/**
+ * Sockect.io to Connect and Disconnect Chat
+ */
+io.on('connection', (socket) => {
+    console.log('New Connection!!!');
 
+    socket.on('disconnect', () => {
+        console.log('Left Connection!!!');
+    });
+});
+
+
+/**
+ * Run routes
+ */
 app.use(router);
 
+/**
+ * Initialize server
+ */
 server.listen(PORT, () => {
-    console.log('Server started on port ${PORT}');
+    console.log(`Server started on port ${PORT}`);
 }); 
