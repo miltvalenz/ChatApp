f5e49b54cffbf1a2a8c419ecf6739532ec3c6714
MiltonValenzuela <miltvalenz@hotmail.com>
2019-11-25 19:53:11 -0400

    Messages and message component changes

--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ScrollToBottom from 'react-scroll-to-bottom';
 
+import Message from '../Message/Message'
+
 import './Messages.css';
 
 const Messages = ({ messages, name }) => {
